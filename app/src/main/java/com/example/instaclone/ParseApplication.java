31d07144af6a7df9874bdee2d26a180ce8b83dd6
Juliana Paul <julianapaul@Julianas-MacBook-Pro.local>
2020-12-04 19:12:27 -0500

    Initial commit

---
+++ app/src/main/java/com/example/instaclone/ParseApplication.java
@@ -0,0 +1,23 @@
+package com.example.instaclone;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class ParseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        ParseObject.registerSubclass(Post.class);
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("XnAGVfYYnz62WtLydJSClPT5RRxCPAGR47IyMJ4x")
+                .clientKey("DtSohyVAeKyuBL2CfhCN9vWMZGsLyvcZkn52ZHEH")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
+}
